10e1b12354993a699c9ee841dbe59641ca880b1c
carrot <carrot.moncher@gmail.com>
2022-05-10 10:28:31 +0800

    fixed typo

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ PKGVER="0.2.4"
 mkdir -v $PKGNAME-$PKGVER &&
 cp -v kbuilder $PKGNAME-$PKGVER/ &&
 cp -v LICENSE $PKGNAME-$PKGVER/ &&
-cp -v doc/documentation.html $PKGNAME_$PKGVER/ &&
+cp -v doc/documentation.html $PKGNAME-$PKGVER/ &&
 tar czvf $PKGNAME-$PKGVER.tar.gz * &&
 archive_digest=$(b2sum $PKGNAME-$PKGVER.tar.gz) &&
 suffix="  $PKGNAME-$PKGVER.tar.gz" &&

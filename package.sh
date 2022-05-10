c2556834f98f0f03e15d15e7338fe8712b29e9fa
carrot <carrot.moncher@gmail.com>
2022-05-10 21:01:22 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -6,6 +6,7 @@
 PKGNAME="kbuilder"
 PKGVER="0.3.0"
 
+pandoc doc/$PKGNAME.1.md -s -t man -o doc/$PKGNAME.1 && 
 gzip -v -9 doc/$PKGNAME.1 &&
 mkdir -v -p tmp/$PKGNAME-$PKGVER &&
 cp -v kbuilder tmp/$PKGNAME-$PKGVER/ &&
